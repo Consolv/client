4fad9c7840388ee110e1c5751cb08bc9d3dddba6
Junsu Min <sunsuking@gmail.com>
2022-05-31 04:57:52 +0900

    _document.tsx 추가 - title 추가 - meta description 추가 - meta naver content 추가

--- components/headers/header.tsx
+++ components/headers/header.tsx
@@ -14,50 +14,53 @@ const routes = [
 const Header: React.FC = () => {
   const router = useRouter();
   return (
-    <div className="w-full fixed bg-white z-40">
-      <div className="max-w-[1640px] w-5/6 m-auto text-black">
-        <div className="flex flex-row justify-between">
-          <div className="my-5">
-            <Image src="/logo.svg" width={227} height={45} />
-          </div>
-          <div className="mb-5 w-full border-b-2 border-black flex justify-end">
-            <div className="w-96 flex flex-row font-bold justify-evenly text-sm ">
-              {routes.map((route, idx) => {
-                if (router.pathname.includes(route.path)) {
-                  return (
-                    <div key={idx} className="px-3 flex justify-center items-center bg-blue-500 text-white  h-full">
-                      <Link href={route.path}>
-                        <a>{route.name}</a>
-                      </Link>
-                    </div>
-                  );
-                } else {
-                  return (
-                    <div key={idx} className="px-3 flex justify-center items-center">
-                      <Link href={route.path}>
-                        <a>{route.name}</a>
-                      </Link>
-                    </div>
-                  );
-                }
-              })}
+    <div className="relative">
+      <div className="w-full h-20 fixed bg-white z-40">
+        <div className="max-w-[1640px] w-5/6 m-auto text-black">
+          <div className="flex flex-row justify-between">
+            <div className="my-5">
+              <Image src="/logo.svg" width={227} height={45} />
+            </div>
+            <div className="mb-5 w-full border-b-2 border-black flex justify-end">
+              <div className="w-96 flex flex-row font-bold justify-evenly text-sm ">
+                {routes.map((route, idx) => {
+                  if (router.pathname.includes(route.path)) {
+                    return (
+                      <div key={idx} className="px-3 flex justify-center items-center bg-blue-500 text-white  h-full">
+                        <Link href={route.path}>
+                          <a>{route.name}</a>
+                        </Link>
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div key={idx} className="px-3 flex justify-center items-center">
+                        <Link href={route.path}>
+                          <a>{route.name}</a>
+                        </Link>
+                      </div>
+                    );
+                  }
+                })}
 
-              <div className="w-6 flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
+                <div className="w-6 flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <div className="w-full h-20" />
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,32 @@
+import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import VideoBox from '@components/box/video-box';
 import Description from '@components/main/description';
 import Plan from '@components/main/plan';
 import Partners from '@components/main/partners';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col">
-      <div className="max-w-[1640px] w-5/6 m-auto">
-        <Description />
-      </div>
-      <div className="bg-blue-400">
+    <React.Fragment>
+      <Head>
+        <title>CONSOLV - 콘솔브</title>
+      </Head>
+      <div className="flex flex-col">
         <div className="max-w-[1640px] w-5/6 m-auto">
-          <Plan />
+          <Description />
+        </div>
+        <div className="bg-blue-400">
+          <div className="max-w-[1640px] w-5/6 m-auto">
+            <Plan />
+          </div>
+        </div>
+        <div className="bg-white">
+          <div className="max-w-[1640px] w-5/6 m-auto">
+            <Partners />
+          </div>
         </div>
       </div>
-      <div className="bg-white">
-        <div className="max-w-[1640px] w-5/6 m-auto">
-          <Partners />
-        </div>
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
